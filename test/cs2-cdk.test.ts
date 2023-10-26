e5a8fbf508fd28d3dab586a2f07a2741f1b51c2f
Rasika Amaratissa <rasika.am@gmail.com>
2023-10-26 17:24:17 +1100

    Initial commit

---
+++ test/cs2-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cs2Cdk from '../lib/cs2-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cs2-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cs2Cdk.Cs2CdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
